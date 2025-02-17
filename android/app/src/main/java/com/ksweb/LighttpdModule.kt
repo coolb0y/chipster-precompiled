fcca7d331f0f4d7e4d19f107b3aae3c7e29a08bd
coolb0y <ankurnarayan2016@gmail.com>
2025-02-18 02:19:33 +0530

    removed loader library dependency

--- android/app/src/main/java/com/ksweb/LighttpdModule.kt
+++ android/app/src/main/java/com/ksweb/LighttpdModule.kt
@@ -1,8 +1,6 @@
 package com.ksweb
 
 import android.util.Log
-import android.os.Build
-import android.widget.Toast
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
@@ -12,7 +10,79 @@ import java.io.File
 class LighttpdModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
     init {
-        System.loadLibrary("lhttpd")  // Load the Lighttpd library
+        // Load all required shared libraries
+        loadLibraries()
+    }
+
+    private fun loadLibraries() {
+        try {
+            // List of all required .so files
+            val libraries = listOf(
+                "libapac.so",
+                "libapr-1.so",
+                "libaprutil-1.so",
+                "libcrypto102u.so",
+                "libcrypto.so",
+                "libc++_shared.so",
+                "libcurl.so",
+                "libexpat.so",
+                "libfreetype.so",
+                "libgd.so",
+                "libiconv.so",
+                "libicudata.so",
+                "libicui18n.so",
+                "libicuio.so",
+                "libicutest.so",
+                "libicutu.so",
+                "libicuuc.so",
+                "libintl.so",
+                "libjpeg.so",
+                "libksweb_1-2.so",
+                "liblber.so",
+                "libldap.so",
+                "liblhttpd.so",  // Lighttpd library
+                "libmod_fastcgi.so",
+                "libngx.so",
+                "libonig.so",
+                "libpcre.so",
+                "libph-5-6-40-api23.so",
+                "libph-7-1-33-api23.so",
+                "libph-7-2-34-api23.so",
+                "libph-7-3-30-api23.so",
+                "libph-7-4-23-api23.so",
+                "libph-8-0-28-api23.so",
+                "libph-8-1-19-api23.so",
+                "libph-8-2-6-api23.so",
+                "libphcgi-5-6-40-api23.so",
+                "libphcgi-7-1-33-api23.so",
+                "libphcgi-7-2-34-api23.so",
+                "libphcgi-7-3-30-api23.so",
+                "libphcgi-7-4-23-api23.so",
+                "libphcgi-8-0-28-api23.so",
+                "libphcgi-8-1-19-api23.so",
+                "libphcgi-8-2-6-api23.so",
+                "libpng16.so",
+                "libsmtp.so",
+                "libsqld-5-6-38-api23.so",
+                "libsqld-5-6-38-api27.so",
+                "libsqld-5-7-34-api27.so",
+                "libsqlite3.so",
+                "libssl102u.so",
+                "libssl.so",
+                "libxml2.so",
+                "libzip.so"
+            )
+
+            // Load each library
+            for (library in libraries) {
+                System.loadLibrary(library.removePrefix("lib").removeSuffix(".so"))
+                Log.d("LighttpdModule", "Loaded library: $library")
+            }
+        } catch (e: UnsatisfiedLinkError) {
+            Log.e("LighttpdModule", "Failed to load a library: ${e.message}")
+        } catch (e: Exception) {
+            Log.e("LighttpdModule", "Unexpected error: ${e.message}")
+        }
     }
 
     @ReactMethod
